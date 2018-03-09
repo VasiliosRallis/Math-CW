57bec2bd45396bda91ada8b20ea25b6df1a81a54
suparnan <sv16@ic.ac.uk>
2018-03-09 12:51:44 +0000

    changed exercise 2 script

--- src/ex3/RLC_script.m
+++ src/ex3/RLC_script.m
@@ -5,8 +5,8 @@ clc;
 %Forcing term
 %Vin = @(t) 5;
 %Vin = @(t) 5*exp(-(t^2)/3);
-%Vin = @(t) square(109*t); needs another add-on
-Vin = @(t) sin(2*pi*5*t);
+Vin = @(t) 5*square(5*2*pi*t); %needs another add-on
+%Vin = @(t) sin(2*pi*5*t);
 
 %Initial conditions
 x1(1) = 500*10^-9;
@@ -15,7 +15,7 @@ x2(1) = 0;
 %Stepsize and final time
 h = 0.0001;
 t(1) = 0;
-tf = 0.1;
+tf = 0.5;
 
 %Circuit characteristics
 R = 250;
@@ -31,6 +31,8 @@ dx2 = @(t, x1, x2) (Vin(t) - (1/C)*x1 - R*x2)/L;
 N = round((tf - t(1)) / h); %nr of steps: (interval size)/(step size)
 t = t(1) : h : N*h;
 
-plot(t, x1, 'b');
+Vout = R*x2;
+plot(t, Vout, 'r');
 hold on;
-plot(t, x2, 'r');
+fplot(Vin);
+xlim([0,0.4]);
